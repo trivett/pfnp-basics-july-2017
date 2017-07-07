1854b3912490783540e8892511357147241cf19b
Vincent Trivett <trivett@gmail.com>
2017-07-06 23:56:53 -0400

    completed temperature app

--- temperature-app/app.js
+++ temperature-app/app.js
@@ -1,13 +1,28 @@
-
-
 // paste your function that converts farenheit to celcius here:
 
-// function here that shows the answer and shows **IF** it is freezing of boiling.
+function farenheitToCelsius(farenheit){
+  let step1 = farenheit - 32;
+  let step2 = step1 * 5;
+  let c = step2 / 9;
+  return c;
+}
 
+// function here that shows the answer and shows whether it is freezing of boiling.
 
-
+function displayAnswer(c){
+  $("#answer").html(c);
+  if (c >= 100){
+    $("#boiling-warning").fadeIn();
+  } else if (c <= 0){
+    $("#freezing-warning").fadeIn();
+  }
+}
 // listen for submit of the form to run the function that converts and the one that 
 
 $("#temperature-conversion-form").on("submit", function(){
-
+  $("#boiling-warning").hide();
+  $("#freezing-warning").hide();
+  let f = $("#farenheit-input").val();
+  let c = farenheitToCelsius(f);
+  displayAnswer(c);
 });
